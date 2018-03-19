aedd97fdae2c36555ebf0a418323db5361cac9d5
Pranav Neyveli <prachess@gmail.com>
2018-03-19 16:10:07 -0400

    added potential ways to expand

--- the real to do list app/FirstViewController.swift
+++ the real to do list app/FirstViewController.swift
@@ -6,14 +6,75 @@
 //  Copyright © 2017 Pranav Neyveli. All rights reserved.
 //
 
+// we can expand by building a backend and making this app much more elegant to use. 
 import UIKit
+// always command click on the DataSource 
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+   
+    var item: [String] = []
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+
+    // what is in te cells itself 
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // we keep as empty array
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        // this sets the items aray into the to do list app  and wrap with parenthesis to avoid the issues of optionals
+        // this part important in general and can be used instead of other stuff as well doe !
+        cell.textLabel?.text = item[indexPath.row]
+
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //lets it appear instantly
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        //items variable empty to make it easier
+        
+        // if statement is used to sucessfully move on and we can set a new array that is entered in the text field  to be permenantly stored
+        
+        //create new aray store that
+        
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            item = tempItems
+            
+        }
+        
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            item.remove(at: indexPath.row)
+            // refreshes the upadated data
+            table.reloadData()
+            // calls foth the permenantly saved value
+            UserDefaults.standard.set(item, forKey: "items")
+            
+        }
+        
+    }
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- the real to do list app/SecondViewController.swift
+++ the real to do list app/SecondViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet var itemField: UITextField!
 
+    @IBAction func addButton(_ sender: Any) {
+
+        
+        //items variable empty to make it easier and store permenantly in this data to use it later
+
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        //create new variable that stores that
+        
+        var item:[String] = []
+        
+        // if statement is used to sucessfully move on and we can set a new array that is entered in the text field  to be permenantly stored
+
+        
+        if let tempItems = itemsObject as? String {
+            
+            item = [tempItems]
+            item.append(itemField.text!)
+            
+            print(item)
+            
+        } else {
+            
+            item = [itemField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(item, forKey: "items")
+        
+        itemField.text = ""
+
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
